e5db890b1efa67c26e932ed585a729bec2add067
Marcelo Melz <marcelomelz1@gmail.com>
2022-08-03 16:09:48 -0300

    Aula 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,5 @@
 
 @section('content')
 
-<iframe title="cad" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=20d0af4a-019a-4193-bf73-27e68ea0601e&autoAuth=true&ctid=ae693f9f-b2b8-4816-af7f-971a747b763b&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+
 @endsection
